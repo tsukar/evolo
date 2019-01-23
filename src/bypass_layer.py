056084c3f42bca624a5aeb480f387fb7a67270c2
Ryotaro Tsukada <tsukada@iba.t.u-tokyo.ac.jp>
2019-01-23 17:25:03 +0900

    Implement get_output_size

--- src/bypass_layer.py
+++ src/bypass_layer.py
@@ -4,5 +4,12 @@ class BypassLayer:
         self.from_index = current_index + int(sections[0].params['layers'])
 
 class ConcatLayer(BypassLayer):
-    def get_output_size(self):
-        pass
+    def get_output_size(self, in_h, in_w, in_c):
+        from_h, from_w = 26, 26
+        from_c = int(self.sections[1].params['filters'])
+        stride = int(self.sections[2].params['stride'])
+
+        out_h = int(from_h / stride)
+        out_w = int(from_w / stride)
+        out_c = int(from_c * (stride ** 2) + in_c)
+        return out_h, out_w, out_c
--- src/simple_layer.py
+++ src/simple_layer.py
@@ -5,8 +5,15 @@ class SimpleLayer:
         self.section = section
 
 class ConvLayer(SimpleLayer):
-    def get_output_size(self):
-        pass
+    def get_output_size(self, in_h, in_w, in_c):
+        size = int(self.section.params['size'])
+        stride = int(self.section.params['stride'])
+        pad = int(self.section.params['pad']) if size > 1 else 0
+
+        out_h = int((in_h + pad * 2 - size) / stride + 1)
+        out_w = int((in_w + pad * 2 - size) / stride + 1)
+        out_c = int(self.section.params['filters'])
+        return out_h, out_w, out_c
 
     @classmethod
     def create(cls):
@@ -20,5 +27,11 @@ class ConvLayer(SimpleLayer):
         return ConvLayer(conv_section)
 
 class MaxPoolLayer(SimpleLayer):
-    def get_output_size(self):
-        pass
+    def get_output_size(self, in_h, in_w, in_c):
+        size = int(self.section.params['size'])
+        stride = int(self.section.params['stride'])
+
+        out_h = int((in_h - size) / stride + 1)
+        out_w = int((in_w - size) / stride + 1)
+        out_c = in_c
+        return out_h, out_w, out_c
